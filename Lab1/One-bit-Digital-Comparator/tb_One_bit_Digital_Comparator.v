1ad87c9f308bb725d0517ba99a3777eab2011849
Ch1hayaAn0n1351 <ch1hayaanon1351@gmail.com>
2025-10-01 00:33:35 +0800

    Update tb_One_bit_Digital_Comparator.v

--- Lab1/One-bit-Digital-Comparator/tb_One_bit_Digital_Comparator.v
+++ Lab1/One-bit-Digital-Comparator/tb_One_bit_Digital_Comparator.v
@@ -6,26 +6,21 @@ module tb_One_bit_Digital_Comparator;
 
     wire a_gt_b, a_eq_b, a_lt_b;
 
-    One_bit_Digital_Comparator uut (
-        .a(a),
-        .b(b),
-        .a_gt_b(a_gt_b),
-        .a_eq_b(a_eq_b),
-        .a_lt_b(a_lt_b)
+   One_bit_Digital_Comparator One_inst (
+    .a(a),
+    .b(b),
+    .a_gt_b(a_gt_b),
+    .a_eq_b(a_eq_b),
+    .a_lt_b(a_lt_b)
     );
 
     initial begin
+	    $monitor("Time = %0t, a = %b, b = %b, a_gt_b = %b, a_eq_b = %b, a_lt_b = %b", $time, a, b, a_gt_b, a_eq_b, a_lt_b);
         a = 0; b = 0; #10;
         a = 0; b = 1; #10;
         a = 1; b = 0; #10;
         a = 1; b = 1; #10;
-
         $stop;
     end
-
-    initial begin
-        $monitor("Time = %0t, a = %b, b = %b, a_gt_b = %b, a_eq_b = %b, a_lt_b = %b", $time, a, b, a_gt_b, a_eq_b, a_lt_b);
-    end
-
 
 endmodule
